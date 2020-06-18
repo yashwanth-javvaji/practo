5f44e8913e148b86db35663bf346c1f0728694cb
Yashwanth Javvaji <yashwanthjavvaji208@gmail.com>
2020-06-18 18:01:49 +0530

    initial commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -381,17 +381,15 @@
     </footer>
     <script>
         $('textarea').val($('textarea').val().trim());
-        $(document).ready(function() {
-            $('.navbar-nav>li>a').on('click', function(){
-                $('.navbar-collapse').collapse('hide');
-            });
-            $(document).click(function (event) {
-                $('.navbar-collapse').collapse('hide');
-            });
-            $('textarea').on('keyup paste', function(){
-                var text = $(this).val();
-                $(this).val(text.trimLeft());
-            });
+        $('.navbar-nav>li>a').on('click', function(){
+            $('.navbar-collapse').collapse('hide');
+        });
+        $(document).click(function (event) {
+            $('.navbar-collapse').collapse('hide');
+        });
+        $('textarea').on('keyup paste', function(){
+            var text = $(this).val();
+            $(this).val(text.trimLeft());
         });
     </script>
 </html>
